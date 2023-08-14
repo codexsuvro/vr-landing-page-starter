88272ac395ad92640a4c3aff25f8d8a4cb11442c
Suvrodeep Debnath <suvrodeepdebnath.official@gmail.com>
2023-08-14 13:32:52 +0530

    Changes

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -6,7 +6,7 @@ const Explore = () => {
       <div className="container mx-auto h-full">
         <div className='h-full bg-explore bg-no-repeat bg-cover p-14 flex flex-col items-start justify-center'>
           <h3 className='text-3xl font-semibold mb-8' data-aos='fade-right' data-aos-delay='800'>Explore product in a new way</h3>
-          <p className='max-w-xs mb-12' data-aos='fade-right' data-aos-delay='1100'>Dive into the future with our immersive SuvroVR newsletter, delivering cutting-edge updates directly to your digital realm.</p>
+          <p className='max-w-xs mb-12' data-aos='fade-right' data-aos-delay='1100'>Dive into the future with our immersive SuVRo newsletter, delivering cutting-edge updates directly to your digital realm.</p>
           {/* Form */}
           <form className='flex flex-col w-full space-y-4 gap-x-4 lg:flex-row lg:space-y-0' data-aos='fade-up' data-aos-offset='200'>
             <input className='bg-gradient-to-t from-[#341d38] to-[#271223] h-12 px-4 outline-none rounded-md' type="text" placeholder='Your email'></input>
